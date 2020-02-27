bf6ee037d8de2832e8514d6c319a5391bf1c2648
malkabsh <malkabsh@dunnsolutions.com>
2020-02-27 15:26:30 -0600

    first commit

---
+++ warbler-server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(express.json());
+
+//all my routes here - they will come later
+app.use(function(req, res, next){
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.listen(PORT, function(){
+    console.log(`Server is starting on port ${PORT}`);
+})
