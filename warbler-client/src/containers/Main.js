f429c58fceebf6cef89748ae2ea383fa97b58a0e
Mohammed-Alkabsh <malkabsh@dunnsolutions.com>
2020-03-06 17:49:16 -0600

    completed setting up the sign in and log code. still more work to complete

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -2,21 +2,33 @@ import React from 'react'
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Homepage from '../components/Homepage';
+import Authform from '../components/Authform';
+import { authUser } from "../store/actions/auth";
 
 const Main = (props) => {
     return (
         <div className="container">
             <Switch>
                 <Route exact path="/" render={props => <Homepage {...props} /> } />
+                <Route exact path="/signin" render={props => {
+                    return (
+                        <Authform onAuth={authUser} buttonText="Log in" heading="Welcome Back." {...props} />
+                    )
+                }} />
+                <Route exact path="/signup" render={props => {
+                    return (
+                        <Authform signUp onAuth={authUser} buttonText="Sign me up!" heading="Join Warbler today!" {...props} />
+                    )
+                }} />
             </Switch>
         </div>
     )
 }
 
-funciton mapStateToProps(state) {
+function mapStateToProps(state) {
     return {
         currentUser: state.currentUser
     }
 }
 
-export default withRouter(connect(mapStateToProps, null))(Main);
+export default withRouter(connect(mapStateToProps, null)(Main));
